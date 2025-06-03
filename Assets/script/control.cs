64ab6aa3adc38f46f6ecafc0805d8d2463f08001
CumtTianxinyi <1306589962@qq.com>
2025-06-03 16:30:51 +0800

    成功初始化智慧教室Unity项目（已移除Temp并添加.gitignore）

---
+++ Assets/script/control.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float sensitivity = 2f;
+
+    private float rotationY = 0f;
+
+    void Update()
+    {
+        // ÒÆ¶¯
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float moveY = 0;
+        if (Input.GetKey(KeyCode.E)) moveY += speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Q)) moveY -= speed * Time.deltaTime;
+        transform.Translate(new Vector3(moveX, moveY, moveZ));
+
+        // Ðý×ª
+        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivity;
+        rotationY = Mathf.Clamp(rotationY, -90, 90);
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+    }
+}
